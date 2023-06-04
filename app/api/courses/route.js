93e156ec8ae8e7877381c10739b2d3dff390bc34
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-04 16:27:06 +0100

    fix: add a post request

--- app/api/courses/route.js
+++ app/api/courses/route.js
@@ -1,7 +1,23 @@
-import { v4 as uuid } from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 import { NextResponse } from 'next/server';
 import courses from './data.json';
 
 export async function GET(request) {
+  return NextResponse.json(courses);
+}
+
+export async function POST(request) {
+  const { title, description, level, link } = await request.json();
+
+  const newCourse = {
+    id: uuidv4(),
+    title,
+    description,
+    level,
+    link,
+  };
+
+  courses.push(newCourse);
+
   return NextResponse.json(courses);
 }
