5344a43381c2840c2da7c266bfe585e9fbb41279
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-04 01:58:30 +0100

    fix: add markup

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,7 +3,18 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='header'>
+      <div className="container">
+        <div className="logo">
+          <Link href='/'>Esteem Designs</Link>
+        </div>
+        <div className="links">
+          <Link href='/about'>About</Link>
+          <Link href='/about/team'>Our Team</Link>
+          <Link href='/code/repos'>Code</Link>
+        </div>
+      </div>
+    </header>
   );
 }
 
