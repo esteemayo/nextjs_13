53966553b7cca1d0b4b999a652b7b53dbd23e6af
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-04 01:53:57 +0100

    fix: modify

--- app/layout.js
+++ app/layout.js
@@ -15,7 +15,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={poppins.className}>{children}</body>
+      <body className={poppins.className}>
+        <main className='container'>{children}</main>
+      </body>
     </html>
   )
 }
