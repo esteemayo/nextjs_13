f41d3f73fe66f747ac7ccbc1792e70b73b2d58a8
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-04 04:37:42 +0100

    feat: fetch the contents

--- components/RepoDirs.jsx
+++ components/RepoDirs.jsx
@@ -1,4 +1,15 @@
-const RepoDirs = ({ name }) => {
+async function fetchRepoContents(name) {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+
+  const res = await fetch(`http://api.github.com/repos/esteemayo/${name}/contents`);
+  const contents = await res.json();
+  return contents;
+}
+
+const RepoDirs = async ({ name }) => {
+  const contents = await fetchRepoContents(name);
+  const dirs = contents.filter((content) => content.type === 'dir');
+
   return (
     <div>RepoDirs</div>
   );
