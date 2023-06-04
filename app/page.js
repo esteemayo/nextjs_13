56d3ba32c5eeede197c883eaa121690089771af9
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-04 16:48:01 +0100

    fix: fetch courses api

--- app/page.js
+++ app/page.js
@@ -6,6 +6,15 @@ const Home = () => {
   const [courses, setCourses] = useState([]);
   const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    (async () => {
+      const res = await fetch('/api/courses');
+      const data = await res.json();
+      setCourses(data);
+      setLoading(false);
+    })();
+  }, []);
+
   return (
     <>
       <h1>Welcome to Esteem Designs</h1>
