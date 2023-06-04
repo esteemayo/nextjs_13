09d3b91537708a29c929c6ea909c0634ba183942
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-04 04:43:18 +0100

    fix: destructure dir object

--- components/RepoDirs.jsx
+++ components/RepoDirs.jsx
@@ -17,6 +17,7 @@ const RepoDirs = async ({ name }) => {
       <h3>Directories</h3>
       <ul>
         {dirs.map((dir) => {
+          const { path, name } = dir;
           return (
             <li key={dir.path}>
               <Link href={`/code/repos/${name}/${dir.path}`}>
