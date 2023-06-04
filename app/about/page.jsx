2bb3a5c8961e2a4ac205c6a0c056be0b0e32f7f5
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-04 02:04:32 +0100

    fix: add markup

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -4,7 +4,14 @@ export const metadata = {
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <h1>About Esteem Designs</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem maiores dolorem facere velit non facilis, sed nobis ex quod suscipit voluptatem minima et veritatis, incidunt fugiat reiciendis dolorum amet molestias!
+        Quia, ipsam. Dicta error nobis modi veniam? Mollitia, nostrum, consectetur minima error eius ea sint perspiciatis quo eum dicta porro autem vel ad rem consequatur expedita. Quidem, modi. Magni, pariatur.
+        Voluptatibus, officiis! Unde eveniet ullam voluptate a itaque id cum? Doloremque, minima architecto ratione sint corrupti ipsam mollitia commodi assumenda voluptates, tempore iste veritatis repellat pariatur itaque quas eius illum.
+      </p>
+    </div>
   );
 }
 
