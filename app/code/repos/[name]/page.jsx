04d9891d32fe1e741c1934de62c6514dd32193ab
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-04 04:21:47 +0100

    fix: add Repo component

--- app/code/repos/[name]/page.jsx
+++ app/code/repos/[name]/page.jsx
@@ -3,8 +3,7 @@ import Repo from "@/components/Repo";
 const RepoPage = ({ params: { name } }) => {
   return (
     <div className='card'>
-      <h2>{name}</h2>
-      <p>Repo Details</p>
+      <Repo name={name} />
     </div>
   );
 }
