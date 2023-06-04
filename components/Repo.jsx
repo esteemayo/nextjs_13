2c89fc954b697b0be18665af2892c07c240dd03d
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-04 04:27:03 +0100

    fix: add markup

--- components/Repo.jsx
+++ components/Repo.jsx
@@ -11,7 +11,24 @@ const Repo = async ({ name }) => {
   const repo = await fetchRepo(name);
 
   return (
-    <div>Repo</div>
+    <>
+      <h2>{repo.name}</h2>
+      <p>{repo.description}</p>
+      <div className='card-stats'>
+        <div className='card-stat'>
+          <FaStar />
+          <span>{repo.stargazers_count}</span>
+        </div>
+        <div className='card-stat'>
+          <FaCodeBranch />
+          <span>{repo.forks_count}</span>
+        </div>
+        <div className='card-stat'>
+          <FaEye />
+          <span>{repo.watchers_count}</span>
+        </div>
+      </div>
+    </>
   );
 }
 
