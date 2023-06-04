ae72f2e2be9b764aa5337b6fab0f1876d85e24bb
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-04 14:19:31 +0100

    fix: create a function to fetch courses api

--- components/Courses.jsx
+++ components/Courses.jsx
@@ -1,5 +1,11 @@
 import Link from 'next/link';
 
+async function fetchCourses() {
+  const res = await fetch('http://localhost:3001/api/courses');
+  const courses = await res.json();
+  return courses;
+}
+
 const Courses = () => {
   return (
     <div>Courses</div>
