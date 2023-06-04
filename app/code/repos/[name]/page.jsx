a6affed56d8b0e4ec058f38cd5334095a7425a77
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-04 04:51:23 +0100

    fix: modify

--- app/code/repos/[name]/page.jsx
+++ app/code/repos/[name]/page.jsx
@@ -2,7 +2,6 @@ import Link from 'next/link';
 
 import Repo from '@/components/Repo';
 import RepoDirs from '@/components/RepoDirs';
-import { Suspense } from 'react';
 
 const RepoPage = ({ params: { name } }) => {
   return (
