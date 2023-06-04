682cad0b76ac5a6043651a43454e231d0a9211ac
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-04 01:49:36 +0100

    fix: add goggle fonts

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,10 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Poppins } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const poppins = Poppins({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+})
 
 export const metadata = {
   title: 'Esteem Designs',
@@ -12,7 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={poppins.className}>{children}</body>
     </html>
   )
 }
